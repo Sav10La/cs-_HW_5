f81cc8647ade8e59fd1ae90dbffcfe3cc464b1d7
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-07-09 14:09:47 +0500

    add Task034 EvenNumbers

---
+++ Task034/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }    
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Вывод масcива");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+void Zadacha34 ()
+{
+    Random random = new Random();
+    int size = 7;
+    int[] array = new int[size];
+    
+    FillArray(array);
+    PrintArray(array);
+    
+    Console.WriteLine();
+    EvenNumbers(array);
+}
+
+Zadacha34();
+
+
